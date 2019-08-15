9696a02a4508fd867106b9e6ebdffc6ac9c8fb8c
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-15 18:59:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length === 0){
-    return 'The line is currently empty.'
-  }
-  else
   for(let i = 0; i < katzDeliLine.length; i++){
     var line = []
-    line.push([i+1]+'. '+ katzDeliLine[i])
-    return 'The line is currently: ' + line
-  }
+    line.push([i+1]+'. '+ katzDeliLine[i])}
+     if(katzDeliLine.length === 0){
+       return 'The line is currently empty.'}
+    else
+       return 'The line is currently: ' + line
+  
 }
